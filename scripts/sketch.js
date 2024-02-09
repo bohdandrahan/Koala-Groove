a12745159fd8bce8fbcb754e38081e0f6419c26f
bohdandrahan <bohdan.drahan@gmail.com>
2024-02-08 19:12:52 -0500

    test other thing on github

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,8 +1,6 @@
 let mic;
 
 function setup() {
-    mic = new p5.AudioIn();
-    mic.start()
     let canvasContainer = document.getElementById('canvas-container');
     let containerWidth = canvasContainer.offsetWidth;
     let containerHeight = canvasContainer.offsetHeight;
@@ -12,5 +10,11 @@ function setup() {
 function draw() {
     background("#feca57")
     // Your drawing logic (if needed)
+}
+
+function StartRecording(){
+    console.log('Start')
+    mic = new p5.AudioIn();
+    mic.start()
 }
 
